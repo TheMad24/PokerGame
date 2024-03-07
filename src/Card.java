2c2129cf00bda6feba2af2283a081bf27e9e1776
Mhamed Merabet <mad.merabet.24@gmail.com>
2024-03-07 15:38:34 +0100

    Added Card class and deck methode

--- src/Card.java
+++ src/Card.java
@@ -1,3 +1,54 @@
 public class Card {
-    
+    int num;
+    String sym;
+    /*
+    clubs (♣),
+    diamonds (♦),
+    hearts (♥)
+    spades (♠)
+     */
+
+    public Card(int num,String sym){
+        this.num = num;
+        this.sym = sym;
+    }
+
+    void showCard(){
+        System.out.println(num+sym);
+    }
+
+    Card[] createDeck(){
+        Card[] d ={} ;
+
+        Card[] deck = new Card[52] ;
+        for (int i=0;i<52;i++){
+            int count = 0;
+            int j=i+1;
+            while (j>13){
+                j = j -13;
+                count++;
+            }
+            if (count == 0){
+                deck[i] = new Card(j,"♣");
+
+            }else if (count == 1){
+                deck[i] = new Card(j,"♦");
+
+            }else if (count == 2){
+                deck[i] = new Card(j,"♥");
+            }else {
+                deck[i] = new Card(j,"♠");
+            }
+        }
+        return deck;
+    }
+
+    public static void main(String[] args) {
+        Card obj = new Card(5,"♣");
+        Card[] arr = obj.createDeck();
+        for(int i=0;i<52;i++){
+
+            arr[i].showCard();
+        }
+    }
 }
